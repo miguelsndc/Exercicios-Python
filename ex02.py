cbbf515c7c7ce74d4c566778f9e2cdbf9fe18345
miguelsndc <67760107+miguelsndc@users.noreply.github.com>
2020-07-14 11:17:27 -0300

    Exercicios e Desafios - Concluido

--- ex02.py
+++ ex02.py
@@ -1,6 +1,13 @@
-def func(*args):
-    args = list(args)
-    args[0] = 20
+def func(*args, **kwargs):
     print(args)
+    print(kwargs)
+    idade = kwargs.get('idade')
+    if idade != None:
+        print(idade)
+    else:
+        print('Não foi possível encontrar a idade.')
 
-func(1, 2, 3, 4, 5, 6, 7)
+
+lista = [1, 2, 3, 4, 5]
+lista2 = [10, 20, 30, 40, 50]
+func(*lista, *lista2, nome='Luiz', sobrenome = 'Miranda', idade = 30)
